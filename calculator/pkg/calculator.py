302a1cf24408ce4c88229dc4033c76d5fed27e31
liledition <liledition@gmail.com>
2025-07-14 14:51:34 +0200

    final?

--- calculator/pkg/calculator.py
+++ calculator/pkg/calculator.py
@@ -9,7 +9,7 @@ class Calculator:
             "/": lambda a, b: a / b,
         }
         self.precedence = {
-            "+": 1,
+            "+": 2,
             "-": 1,
             "*": 2,
             "/": 2,
